0e6d1c99e717e14debfd49033d6857e747d87c4f
Josh Padilla <jdp237@nau.edu>
2020-08-25 15:14:24 -0700

    adding html, css, javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+// Constants
+const generateBtn = document.querySelector("#generate");
+
+// Functions
+
+/**
+ * writes a generated password to the #password
+ */
+function writePassword() {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+/**
+ * generate a password based on certain criteria
+ */
+function generatePassword(){
+
+  // Password is currently blank! We need to make a better one
+  let password = "";
+
+
+
+
+
+  return password;
+}
+
+// Main Process
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);

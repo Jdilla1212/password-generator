d702e71b1d6002998d05c6c0eb63240f80686aad
Josh Padilla <jdp237@nau.edu>
2020-08-29 16:29:02 -0700

    Commiting the finished product

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # password-generator
+
+# I didn't commit often enough, and I only just recently realized I was supposed to commit often.  Apologies.
+
+# description, section for languages and concepts, screenshot of project running, link to deployed page, info on myself like email
+
+# This is a password generator that asks for certain criteria from the user.  The generator functions to give at least 1 of the criteria selected by the user and the fills in the rest of the password length with randomly selected characters from the list of possible characters.
+
+# I only modified the javascript to give it the desired functionality.  HTML and CSS were provided beforehand.
+
+# https://jdilla1212.github.io/password-generator/
+
+# I can be reached via email at jdp237@nau.edu
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Constants
 const generateBtn = document.querySelector("#generate");
+const alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const numerical = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharacters = ["!", "@", "#", "$", "%", "&"];
 
 // Functions
 
@@ -13,22 +17,97 @@ function writePassword() {
   passwordText.value = password;
 }
 
-/**
- * generate a password based on certain criteria
- */
-function generatePassword(){
+// /**
+//  * generate a password based on certain criteria
+//  */
+function generatePassword() {
 
+  //   //input criteria from user
+  //   //If any condition is true, include the appropriate array
+  let possibleCharacterOptions = []
+
+
+  const lengthChoice = window.prompt("How many characters (between 8 and 128) would you like your password to be?");
+  if (lengthChoice < 8 || lengthChoice > 128) {
+    alert("You picked a no-no");
+    return "Try again"
+  }
+  // console.log(lengthChoice);
+
+  const includeSpecialCharacters = window.confirm("Would you like to use special characters?");
+  // console.log(includeSpecialCharacters);
+
+  const includeUpperCase = window.confirm("Would you like to include upper case letters?");
+  // console.log(includeUpperCase);
+
+  const includeLowerCase = window.confirm("Would you like to include lower case letters?");
+  // console.log(includeLowerCase);
+
+  const includeNumbers = window.confirm("Would you like to use numbers?");
+  // console.log(includeNumbers);
   // Password is currently blank! We need to make a better one
   let password = "";
+  let hasAddedSpecialChar = false;
+  let hasAddedLowerCase = false;
+  let hasAddedUpperCase = false;
+  let hasAddedNumber = false;
 
+  if (includeUpperCase) {
+    possibleCharacterOptions.push(...alphaUpper);
+  }
+  if (includeLowerCase) {
+    possibleCharacterOptions.push(...alphaLower);
+  }
+  if (includeNumbers) {
+    possibleCharacterOptions.push(...numerical);
+  }
+  if (includeSpecialCharacters) {
+    possibleCharacterOptions.push(...specialCharacters);
+  }
+  console.log(possibleCharacterOptions);
 
-
+  //create for loop that'll repeat number of lengthChoice times
+  for (let i = 0; i < lengthChoice; i++) {
+    console.log(password);
+    if (includeSpecialCharacters && !hasAddedSpecialChar) {
+      password = password + specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+      hasAddedSpecialChar = true;
+      continue;
+    }
+    if (includeLowerCase && !hasAddedLowerCase) {
+      password = password + alphaLower[Math.floor(Math.random() * alphaLower.length)];
+      hasAddedLowerCase = true;
+      continue;
+    }
+    if (includeUpperCase && !hasAddedUpperCase) {
+      password = password + alphaUpper[Math.floor(Math.random() * alphaUpper.length)];
+      hasAddedUpperCase = true;
+      continue;
+    }
+    if (includeNumbers && !hasAddedNumber) {
+      password = password + numerical[Math.floor(Math.random() * numerical.length)];
+      hasAddedNumber = true;
+      continue;
+    }
+    password = password + possibleCharacterOptions[Math.floor(Math.random() * possibleCharacterOptions.length)]
+  }
 
 
   return password;
+
+
 }
+//math.random out of possible char options
+//concatenate random chars
 
-// Main Process
 
-// Add event listener to generate button
+
+
+
+
+// // Main Process
+
+// // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
